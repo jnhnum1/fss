eca3e7582bff6acf45f2dc4d06723af9e0ad20eb
Pritish Uday Kamath <pritish@mit.edu>
2014-04-19 19:03:44 -0400

    Created non-functional file_watcher

---
+++ Justin_Pritish/file_watcher.go
@@ -0,0 +1,26 @@
+package main
+
+import "code.google.com/p/go.exp/inotify"
+import "fmt"
+
+func main() {
+  
+  watcher, err := inotify.NewWatcher()
+  if err != nil {
+    fmt.Println("inotify error:", err) //log.Fatal(err)
+  }
+  err = watcher.Watch("./watch_folder")
+  if err != nil {
+    fmt.Println("Watch error:", err) //log.Fatal(err)
+  }
+  for {
+    select {
+    case ev := <-watcher.Event:
+        log.Println("event:", ev)
+    case err := <-watcher.Error:
+        log.Println("error:", err)
+    }
+  }
+  
+  fmt.Println("Hello, playground")
+}
