c359673b59848275f75e38ba5f2a40569e6bf294
Zach <zek8922@gmail.com>
2014-04-28 16:06:43 -0400

    added watch files to test

--- TnT/test_test.go
+++ TnT/test_test.go
@@ -6,6 +6,8 @@ import (
   "os"
   "strconv"
   "testing"
+  "encoding/gob"
+  "code.google.com/p/go.exp/inotify"
 )
 
 func port(tag string, host int) string {
@@ -60,3 +62,62 @@ func TestGetFile(t *testing.T) {
   
   fmt.Println("  ... Passed")
 }
+
+func TestWatchFolder(t *testing.T) {
+  root_folder := "/home/zek/fss/roots/root0"
+  //dirname := "." + string(filepath.Separator) + root_folder + string(filepath.Separator)
+  dirname := root_folder
+
+  //setup the watch
+  fst := new(FStree)
+  fst.Tree = make(map[string]*FSnode)
+  fst.Tree[dirname] = new(FSnode)
+  fst.Tree[dirname].Name = root_folder
+  fst.Tree[dirname].Depth = 0
+  fst.Tree[dirname].Children = make(map[string]bool)
+
+  FST_create(dirname, 0, fst)
+  f, err := os.OpenFile("FST_watch", os.O_WRONLY | os.O_CREATE, 0777)
+  if err != nil {
+    log.Println("Error opening file:", err)
+  }
+
+  encoder := gob.NewEncoder(f)
+  encoder.Encode(fst)
+  f.Close()
+  fmt.Println("FST_watch dumped!")
+
+  //Test the watch here
+  fmt.Println(dirname)
+  f, err = os.Open("FST_watch")
+  defer f.Close()
+  if err != nil {
+      log.Println("Error opening file:", err)
+  }
+  var fst1 FStree
+  decoder := gob.NewDecoder(f)
+  decoder.Decode(&fst1)
+
+  watcher, err := inotify.NewWatcher()
+  if err != nil {
+      log.Fatal(err)
+  }
+
+  FST_parse_watch(&fst1, dirname, watcher)
+
+
+  for {
+      select {
+      case ev := <-watcher.Event:
+          log.Println("event:", ev)
+      case err := <-watcher.Error:
+          log.Println("error:", err)
+      }
+  }
+}
+
+
+
+
+
+
