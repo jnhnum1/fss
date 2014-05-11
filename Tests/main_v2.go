8487fd8bf227ecf7fff8e9e0ffa970909c956e7e
Zachary Kabelac <zek@30-9-163.wireless.csail.mit.edu>
2014-05-10 21:08:40 -0400

    Watcher

--- Tests/main_v2.go
+++ Tests/main_v2.go
@@ -8,7 +8,7 @@ import (
   	"strconv"
   	"TnT_v2"
   	"path/filepath"
-    "rand"
+    "math/rand"
 )
 
 const (
@@ -195,7 +195,7 @@ func main() {
     fmt.Println("Test: Randomly Create Directories and Files ...")
 
     rand.Seed(42)
-    action_list := [] string {
+    action_list := [] string{
         "Create_Dir",
         "Delete_Dir",
         "Create_File",
