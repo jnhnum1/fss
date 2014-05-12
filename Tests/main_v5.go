e2f34b6aed225497d14f06bf25330ce7fb5c93c8
Zachary Kabelac <zek@30-9-163.wireless.csail.mit.edu>
2014-05-11 22:31:10 -0400

    tess

--- Tests/main_v5.go
+++ Tests/main_v5.go
@@ -262,6 +262,8 @@ func EditDirectory(num_actions int, nservers int, me int, root string, tnt *TnT_
 
     var text string
 
+    text = strconv.Itoa(info)
+
     if _, err = f.WriteString(text); err != nil {
         panic(err)
     }
