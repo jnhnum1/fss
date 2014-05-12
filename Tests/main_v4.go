48eb0f420824a5f00922ce37abbe43924bc4595b
Zachary Kabelac <zek@30-9-163.wireless.csail.mit.edu>
2014-05-11 20:01:43 -0400

    test

--- Tests/main_v4.go
+++ Tests/main_v4.go
@@ -310,10 +310,10 @@ func main() {
     }
     //c := make(chan int)
     stop_all := make(chan int)
-    for j:=1;j<1000;j++{
+    for j:=1;j<100;j++{
       for i:=0; i<nservers; i++ {
 
-          go EditDirectory(200, nservers, i, common_root+strconv.Itoa(i)+"/", tnts[i],c[i], stop_all)
+          go EditDirectory(100, nservers, i, common_root+strconv.Itoa(i)+"/", tnts[i],c[i], stop_all)
       }
       for i:=0;i<nservers;i++{
           <-c[i]
