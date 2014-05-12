a3bca22526c897e88256052e0feb4f7241fe8a64
Zachary Kabelac <zek@30-9-163.wireless.csail.mit.edu>
2014-05-11 22:50:32 -0400

    tess

--- Tests/main_v5.go
+++ Tests/main_v5.go
@@ -283,7 +283,7 @@ func main() {
 
     SyncAll(nservers, tnts)
 
-    for j:=1;j<5;j++{
+    for j:=0;j<50;j++{
       for i:=0; i<nservers; i++ {
 
           go EditDirectory(100, nservers, i, common_root+strconv.Itoa(i)+"/", tnts[i],c[i], stop_all, &creates[i], &deletes[i])
