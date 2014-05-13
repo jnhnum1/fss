8b97fdcfe1e57163c6eda156917355a833a7c0ae
Zachary Kabelac <zek@30-9-163.wireless.csail.mit.edu>
2014-05-12 21:40:03 -0400

    main final

--- Tests/main_final.go
+++ Tests/main_final.go
@@ -1,11 +1,11 @@
 package main
 
 import (
-  	"fmt"
-  	"os"
-  	"strconv"
-  	"TnT_final"
-  	"path/filepath"
+    "fmt"
+    "os"
+    "strconv"
+    "TnT_v2_2"
+    "path/filepath"
     "math/rand"
     "time"
     "io/ioutil"
@@ -13,24 +13,24 @@ import (
 )
 
 const (
-  	common_root = "../roots/nest"
+    common_root = "../roots/nest"
 )
 
 func port(tag string, host int) string {
-  	s := "/var/tmp/824-"
-  	s += strconv.Itoa(os.Getuid()) + "/"
-  	os.Mkdir(s, 0777)
-  	s += "tnt-"
-  	s += strconv.Itoa(os.Getpid()) + "-"
-  	s += tag + "-"
-  	s += strconv.Itoa(host)
-  	return s
+    s := "/var/tmp/824-"
+    s += strconv.Itoa(os.Getuid()) + "/"
+    os.Mkdir(s, 0777)
+    s += "tnt-"
+    s += strconv.Itoa(os.Getpid()) + "-"
+    s += tag + "-"
+    s += strconv.Itoa(host)
+    return s
 }
 
-func cleanup(tnts []*TnT_final.TnTServer) {
-  	for i:=0; i < len(tnts); i++ {
-    	tnts[i].Kill()
-  	}
+func cleanup(tnts []*TnT_v2_2.TnTServer) {
+    for i:=0; i < len(tnts); i++ {
+      tnts[i].Kill()
+    }
 }
 
 func DFT(dirname string, depth int ,str string)string {
@@ -50,11 +50,11 @@ func DFT(dirname string, depth int ,str string)string {
            str=str+fi.Name()
         }
         if fi.IsDir() {
-  			str=str+fi.Name()
+        str=str+fi.Name()
             str=DFT(dirname+fi.Name()+string(filepath.Separator), depth+1,str)
         }else{
-		data,_:=ioutil.ReadFile(dirname+fi.Name())
-        	str=str+string(data)
+    data,_:=ioutil.ReadFile(dirname+fi.Name())
+          str=str+string(data)
         }
     }
     return str
@@ -88,28 +88,28 @@ func parent(path string) string {
     return path[0:end+1]
 }
 
-func setup(tag string, nservers int) ([]*TnT_final.TnTServer, func()) {
+func setup(tag string, nservers int) ([]*TnT_v2_2.TnTServer, func()) {
 
-  	var peers []string = make([]string, nservers)
-  	var tnts []*TnT_final.TnTServer = make([]*TnT_final.TnTServer, nservers)
+    var peers []string = make([]string, nservers)
+    var tnts []*TnT_v2_2.TnTServer = make([]*TnT_v2_2.TnTServer, nservers)
 
-  	for i:=0; i<nservers; i++ {
-    	peers[i] = port(tag, i)
-  	}
+    for i:=0; i<nservers; i++ {
+      peers[i] = port(tag, i)
+    }
 
-  	for i:=0; i<nservers; i++ {
+    for i:=0; i<nservers; i++ {
 
         os.RemoveAll(common_root+strconv.Itoa(i)+"/")
         os.Remove("../Tests/WatchLog"+strconv.Itoa(i))
         os.Mkdir(common_root+strconv.Itoa(i)+"/", 0777)
-  	    tnts[i]=TnT_final.StartServer(peers,i, common_root+strconv.Itoa(i)+"/", "WatchLog"+strconv.Itoa(i), common_root+"tmp"+strconv.Itoa(i)+"/", true)
-  	}
+        tnts[i]=TnT_v2_2.StartServer(peers,i, common_root+strconv.Itoa(i)+"/", "WatchLog"+strconv.Itoa(i), common_root+"tmp"+strconv.Itoa(i)+"/", true)
+    }
 
-  	clean := func() { (cleanup(tnts)) }
-  	return tnts, clean
+    clean := func() { (cleanup(tnts)) }
+    return tnts, clean
 }
 
-func SyncAll(nservers int, tnts []*TnT_final.TnTServer){
+func SyncAll(nservers int, tnts []*TnT_v2_2.TnTServer){
 
     for i := 0; i<nservers; i++ {
         if i != 0 {
@@ -139,7 +139,7 @@ func SyncAll(nservers int, tnts []*TnT_final.TnTServer){
 
 }
 
-func EditDirectory(num_actions int, nservers int, me int, root string, tnt *TnT_final.TnTServer, c chan int, stop_all chan int){
+func EditDirectory(num_actions int, nservers int, me int, root string, tnt *TnT_v2_2.TnTServer, c chan int, stop_all chan int){
     fmt.Println("Edit Directory ...")
 
     rand.Seed( time.Now().UTC().UnixNano())
@@ -249,15 +249,70 @@ func EditDirectory(num_actions int, nservers int, me int, root string, tnt *TnT_
 
 func main() {
 
-  	const nservers = 5
+    const nservers = 5
 
 
-  	//printfiles(nservers)
+    //printfiles(nservers)
 
-  	tnts, clean := setup("sync", nservers)
-  	fmt.Println(tnts)
-  	defer clean()
+    tnts, clean := setup("sync", nservers)
+    fmt.Println(tnts)
+    defer clean()
   
+    // fmt.Println("Test: Single File Syncing ...")
+
+    
+    // fmt.Println("Enter -1 to quit the loop")
+    // a := 100
+    // b := 100
+    // for a >= 0 && b >= 0 {
+
+   //     fmt.Printf("Sync? Enter (who) and (from): ")
+   //     n, err := fmt.Scanf("%d %d\n", &a, &b)
+   //     if err != nil {
+   //         fmt.Println("Scanf error:", n, err)
+   //     }
+
+   //     if 0 <= a && a < nservers && 0 <= b && b < nservers && a != b {
+   //         tnts[a].SyncWrapper(b,"./")
+   //       //printfiles(nservers)
+   //     }
+
+   //   fmt.Println("-----------------------------")
+    // }
+    
+
+    
+ //   var test_count int = 0
+ //   fmt.Println("Test: Sync File ...")  
+
+ //   //Create file on nest0
+ //   file_name := common_root+strconv.Itoa(0)+"/"+strconv.Itoa(test_count)+".txt"
+ //   os.Create(file_name)
+
+ //   //Sync all servers
+  // SyncAll(nservers, tnts)
+
+ //   //Check that file is in nest1, Open throws error is file does not exist
+ //   _,err := os.Open(common_root+strconv.Itoa(1)+"/"+strconv.Itoa(test_count)+".txt")
+ //   if err != nil {
+ //     fmt.Println("File Transfer Failed")
+ //     os.Exit(1)
+ //   }
+
+
+ //   fmt.Println("Test: Sync Folder ...")
+ //   test_count++
+ //   //Create folder on nest0
+ //   folder_name := common_root+strconv.Itoa(0)+"/"+strconv.Itoa(test_count)+"/"
+ //   os.Mkdir(folder_name, 0777)
+
+ //    SyncAll(nservers, tnts)
+
+ //   _,err = os.Open(common_root+strconv.Itoa(2)+"/"+strconv.Itoa(test_count)+"/")
+ //   if err != nil {
+ //     fmt.Println("Folder Transfer Failed")
+ //     os.Exit(1)
+ //   }
 
     SyncAll(nservers, tnts)
 
@@ -291,7 +346,7 @@ func main() {
     SyncAll(nservers, tnts)
     var h [nservers]uint32
     for i:=0;i<nservers;i++{
-    	fmt.Println(hash(DFT(common_root+strconv.Itoa(i)+"/",0,"")))
+      fmt.Println(hash(DFT(common_root+strconv.Itoa(i)+"/",0,"")))
       h[i]=hash(DFT(common_root+strconv.Itoa(i)+"/",0,""))
       if(h[i]!=h[0]){
         fmt.Println("Failed");
